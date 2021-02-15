57f026e484a72435cd781f2879a63ce9248bc8e8
Callum Ross <callumr.ross97@gmail.com>
2021-02-15 19:01:46 +0000

    tidy up folders

--- groupH_login_page/logout.php
+++
@@ -1,8 +0,0 @@
-<?php
-// Resume the session
-session_start();
-//Destroy the session
-session_destroy();
-//Redirect to home page
-header("Location: loginPage.php");
-?>
--- groupH_login_page/userAccounts.php
+++
@@ -1,5 +0,0 @@
-<?php
-// Key value pair array holds all username and password combinations
-    $userAccounts = array("Baz"=>"letmein","Dave"=>"thekey",
-                                            "Keith"=>"password");
-?>

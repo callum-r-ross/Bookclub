1e8e95e9b0a8d0c9d3cdd8bd2209972c5365954a
Callum Ross <callumr.ross97@gmail.com>
2021-02-15 15:43:57 +0000

    connected

--- Lab 8/labB/marvelStudios.php
+++ Lab 8/labB/marvelStudios.php
@@ -1,3 +1,19 @@
 <?php
+// Ensure database is connected
+include("connection.php");
+
 echo "<h1>Marvel Movies created by Marvel Studios</h1>";
+$m = "Marvel Studios";
+// Sql query
+$sql = "SELECT title FROM marvelmovies";
+
+// Variable which holds result of the search
+$result = $db->query($sql_query);
+
+while($row = $result -> fetch_array()){
+    echo "HI";
+}
+
+$result->close();
+$db->close();
 ?>

1c5a56a2a2f85fd8f8a9be8ecd8ca14178ea5a2b
Callum Ross <callumr.ross97@gmail.com>
2021-03-02 17:41:07 +0000

    d

--- Coursework/addBook.php
+++ Coursework/addBook.php
@@ -5,6 +5,7 @@ if(! isSet($_POST)){
     header("Location: index.php");
     die();
 } else {
+    // Form post variables
     $bookTitle = $_POST["bookTitle"];
     $authorFirstName = $_POST["authorFirstName"];
     $authorLastName = $_POST["authorLastName"];
@@ -14,8 +15,8 @@ if(! isSet($_POST)){
     $bookRecommended = $_POST["bookRecommended"];
     $bookGenre = $_POST["bookGenre"];
 
+    // Uploaded image variables
     $file = $_FILES['file'];
-
     $fileName = $_FILES['file']['name'];
     $fileTmpName = $_FILES['file']['tmp_name'];
     $fileSize = $_FILES['file']['size'];
@@ -30,12 +31,7 @@ if(! isSet($_POST)){
     if(in_array($fileActualExt,$allowed)){
         if($fileError === 0){
             if($fileSize < 500000){
-                $sql_query = "INSERT INTO bookReviews (bookTitle,authorFirstName,authorLastName,bookPublisher,bookSummary,bookRating,bookRecommended,bookGenre) VALUES ('$bookTitle','$authorFirstName','$authorLastName','$bookPublisher','$bookSummary','$bookRating','$bookRecommended','$bookGenre')";
-
-                $fileNameNew = uniqid('',true).".".$fileActualExt;
-                $fileDestination = 'uploads/'.$fileNameNew;
-                move_uploaded_file($fileTmpName,$fileDestination);
-                header("Location: index.php?uploadsuccess");
+                $sql_query = "INSERT INTO bookReviews (bookTitle,authorFirstName,authorLastName,bookPublisher,bookSummary,bookRating,bookRecommended,bookGenre) VALUES ('$bookTitle','$authorFirstName','$authorLastName','$bookPublisher','$bookSummary','$bookRating','$bookRecommended','$bookGenre')";               
             } else {
                 echo "Error";
             }
@@ -49,6 +45,10 @@ if(! isSet($_POST)){
 
 
     if(mysqli_query($db,$sql_query)){
+        $fileNameNew = mysqli_insert_id($db).".".$fileActualExt;
+                $fileDestination = 'uploads/'.$fileNameNew;
+                move_uploaded_file($fileTmpName,$fileDestination);
+                header("Location: index.php?uploadsuccess");
     } else {
         echo "Error, book not added";
     }
